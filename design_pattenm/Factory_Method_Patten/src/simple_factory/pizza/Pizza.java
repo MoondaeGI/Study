ff95a4d7f27f4cf9951ed07feff891ad76dab3d9
MoondaeGI <ansgur119977@gmail.com>
2022-06-29 18:30:40 +0900

    Delete wrong directory Design_Pattenm

--- design_pattenm/Factory_Method_Patten/src/simple_factory/pizza/Pizza.java
+++
@@ -1,8 +0,0 @@
-package simple_factory.pizza;
-
-public interface Pizza {
-    public void prepare();
-    public void bake();
-    public void cut();
-    public void box();
-}
--- design_pattenm/Factory_Method_Patten/src/simple_factory/pizzaStore/PizzaStore.java
+++
@@ -1,8 +0,0 @@
-package simple_factory.pizzaStore;
-
-import simple_factory.pizza.Pizza;
-import simple_factory.simplePizzaFactory.SimplePizzaFactory;
-
-public interface PizzaStore {
-    public Pizza orderPizza(String type);
-}
