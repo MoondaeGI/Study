2b1dab3c07f23f28324b732e23711845d0ad694b
MoondaeGI <ansgur119977@gmail.com>
2022-07-15 15:12:42 +0900

    Add composite pattern

---
+++ design_patten/Composite_Patten/src/waitress/Waitress.java
@@ -0,0 +1,15 @@
+package waitress;
+
+import menuComponent.MenuComponent;
+
+public class Waitress {
+    MenuComponent allMenus;
+
+    public Waitress(MenuComponent allMenus) {
+        this.allMenus = allMenus;
+    }
+
+    public void printMenu() {
+        allMenus.print();
+    }
+}
